32b8e948d2ff144a21dc148acf31031cf6f07d7a
Naman <namanagrawal1895@gmail.com>
2025-08-11 20:43:19 +0530

    working on channels listing ui

--- src/modules/servers/sections/channels-section.tsx
+++ src/modules/servers/sections/channels-section.tsx
@@ -5,11 +5,11 @@ import { ErrorBoundary } from "react-error-boundary";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import ServerHeader from "../components/server-header";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Loader2Icon } from "lucide-react";
+import { Edit, Hash, Loader2Icon, Plus, Trash2 } from "lucide-react";
 import ErrorComponent from "@/components/custom/error-box";
+import { Input } from "@/components/ui/input";
 
 interface ChannelsSectionProps {
   serverId: string;
@@ -42,9 +42,13 @@ const ChannelsSectionSuspense = ({ serverId }: ChannelsSectionProps) => {
     trpc.server.getOne.queryOptions({ serverId })
   );
 
-  // const { data: channels } = useSuspenseQuery(
-  //   trpc.channel.getMany.queryOptions({ serverId })
-  // );
+  const { data: channels } = useSuspenseQuery(
+    trpc.channel.getMany.queryOptions({ serverId })
+  );
+
+  const textChannels = channels.filter((channel) => channel.type === "TEXT");
+  const audioChannels = channels.filter((channel) => channel.type === "AUDIO");
+  const videoChannels = channels.filter((channel) => channel.type === "VIDEO");
 
   // const router = useRouter();
 
@@ -55,7 +59,7 @@ const ChannelsSectionSuspense = ({ serverId }: ChannelsSectionProps) => {
   return (
     <div
       className={cn(
-        "h-full w-full sm:w-64 md:w-72 flex flex-col gap-2 bg-[#ececec] dark:bg-[#222222] items-center",
+        "h-full w-full sm:max-w-64 md:max-w-72 flex flex-col gap-2 bg-[#ececec] dark:bg-[#222222] items-center",
         hasChannel ? "hidden sm:flex" : ""
       )}
     >
@@ -69,10 +73,99 @@ const ChannelsSectionSuspense = ({ serverId }: ChannelsSectionProps) => {
         userId={data.userId}
       />
 
-      {/* <div className="h-[1px] bg-blue w-full"></div> */}
+      {/* Todo: change properly later */}
+      <Input
+        className="w-[100%] mx-4"
+        placeholder="Search channels / members [Ctrl+K]"
+      />
 
-      {/* Create channels ui */}
-      {/* {JSON.stringify(channels)} */}
+      {/* Render the channels */}
+      <div className="flex flex-col gap-6 mt-4 w-[100%] px-2">
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-muted-foreground">Text Channels</p>
+            <div className="p-1 rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+              <Plus size={16} />
+            </div>
+          </div>
+          {textChannels.map((channel, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center hover:bg-muted py-1 rounded-md"
+            >
+              <div className="text-muted-foreground flex gap-2 items-center px-1">
+                <Hash size={18} />
+                <span className="line-clamp-1">
+                  {/* Todo: breaking on longer texts */}
+                  asdnbassadasdsadsadsahdb asd sasdsa
+                </span>
+              </div>
+              <div className="flex">
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Edit size={14} />
+                </div>
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Trash2 size={14} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-muted-foreground">Audio Channels</p>
+            <div className="p-1 rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+              <Plus size={16} />
+            </div>
+          </div>
+          {audioChannels.map((channel, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center hover:bg-muted py-1 rounded-md"
+            >
+              <div className="text-muted-foreground flex gap-2 items-center">
+                <Hash size={20} />
+                <span className="line-clamp-1">{channel.name}</span>
+              </div>
+              <div className="flex">
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Edit size={14} />
+                </div>
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Trash2 size={14} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-muted-foreground">Video Channels</p>
+            <div className="p-1 rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+              <Plus size={16} />
+            </div>
+          </div>
+          {videoChannels.map((channel, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center hover:bg-muted py-1 rounded-md"
+            >
+              <div className="text-muted-foreground flex gap-2 items-center">
+                <Hash size={20} />
+                <span className="line-clamp-1">{channel.name}</span>
+              </div>
+              <div className="flex">
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Edit size={14} />
+                </div>
+                <div className="p-[6px] rounded-full hover:bg-[#e5e5e5] dark:hover:bg-[#2e2e2e] text-muted-foreground">
+                  <Trash2 size={14} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
       {/* <Button onClick={() => router.push(`/server/${serverId}/channel/123`)}>
         Click
